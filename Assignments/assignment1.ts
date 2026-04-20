53fcbae2156e344446ca3ccac3d615f0dfa15422
CHEGIREDDY SAI SIREESHA REDDY <sireeshareddy5008@gmail.com>
2026-04-20 17:05:58 +0530

    Assignment 1

---
+++ Assignments/assignment1.ts
@@ -0,0 +1,86 @@
+//1. Temperature of a city in degrees Celsius: 25.5 
+let Temp:number = 25.5;
+console.log(`Temperature of a city in degrees Celsius: ${Temp}°C`);
+
+// 2. Whether a customer has placed an order: true or false 
+let hasPlacedOrder: boolean = true;
+
+console.log(`Has the customer placed an order: ${hasPlacedOrder}`);
+
+ // 3. Person's phone number: "123-456-7890" 
+ let str1: string = "123-456-7890";
+ console.log(`Person's phone number: ${str1}`);
+
+ // 4. Amount of money in a customer's bank account: 1000.50 
+ let num1: number = 1000.50;
+ console.log(`Amount of money in a customer's bank account: ${num1}`);
+
+ //5. Person's email address: "john.doe@example.com" 
+ let str2: string = "john.doe@example.com";
+ console.log(`Person's email address: ${str2}`);
+
+ //6.Coordinates of a location (latitude, longitude): 37.7749, -122.4194 
+ let cod1:number = 37.7749
+ let cod2:number = -122.4194 
+ console.log(`Coordinates of a location (latitude, longitude): ${cod1} , ${cod2}`);
+
+ //7. Person's marital status: true or false 
+ let MS:boolean=true;
+ console.log(`Person's marital status:${MS}`);
+
+// 8.Person's occupation: "Software Engineer" 
+let PO:string = "Software Engineer" ;
+console.log(`Person's occupation: ${PO}`);
+
+//9. Person's favourite colour: "Blue" 
+let PFC:string="Blue";
+console.log(`Person's favourite colour: ${PFC}`);
+
+//10.Current year: 2023 
+let currentYear: number = new Date().getFullYear();
+let requiredYear: number = currentYear - 3;
+console.log(`Current year: ${requiredYear}`);
+
+//11.Number of followers on a social media platform: 1,000,000 
+let followers:string= "1,000,000"
+console.log(`Number of followers on a social media platform: ${followers}`);
+
+// 12.Rating of a movie: 7.5 
+let ROFM:number=7.5;
+console.log(`Rating of a movie: ${ROFM}`);
+
+//13.Person's blood type: 'A' 
+let BT:string= 'A';
+console.log(`Person's blood type: ${BT}`);
+
+//14.Title of a book: "To Kill a Mockingbird" 
+let TOB:string="To Kill a Mockingbird" ;
+console.log(`Title of a book: ${TOB}`);
+
+//15.Number of employees in a company: 500 
+let NOE:number= 500;
+console.log(`Number of employees in a company ${NOE}`);
+
+//16.Time of an event: 2:30 PM 
+let TOE:string = "2:30 PM";
+console.log(`Time of an event: ${TOE}`);
+
+//17.Name of a country: "United States" 
+let NOC:string="United States" 
+console.log(`Name of a country: ${NOC}`);
+
+//18.Person's eye color: "Brown" 
+let PEC:string="Brown" 
+console.log(`Person's eye color ${PEC}`);
+
+//19.Person's birthplace: "New York City" 
+let PBP:string="New York City" ;
+console.log(`Person's birthplace: ${PBP}`);
+
+//20. Distance between two cities: 200.5
+let DBTC:number=200.5;
+console.log(`Distance between two cities: ${DBTC}`);
+
+
+
+
